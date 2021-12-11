a7691777c6f5baebd204580a5e87456116f85f90
phuc <nguyentienphuc1991@gmail.com>
2021-12-12 01:51:33 +0700

    update code

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 mkdir -p backup_images
-mv besties/public/* backup_images
+cp besties/public/* backup_images
 sudo rm -rf besties
 git clone -b master https://github.com/nguyentienphuc/strapi-besties.git besties
  
 cd besties
 npm install
 cd ../
-mv backup_images/* besties/public
+cp backup_images/* besties/public
 cd besties
 
 npm run develop
