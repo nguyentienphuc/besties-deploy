d0bb4b4fb97e743212858462a27271b1731c73e0
phuc <nguyentienphuc1991@gmail.com>
2021-12-11 22:11:02 +0700

    update code

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
+mkdir -p public
 mv besties/public/* public
 sudo rm -rf besties
 git clone -b master https://github.com/nguyentienphuc/strapi-besties.git besties
-sudo rm -rf /besties/public 
-mv public/* besties/public 
+sudo rm -rf besties/public 
+mv public/* besties/public
+sudo rm -rf public 
 cd besties
 npm install
 npm run develop
