db6a11ea6520230f145227e2ba48c34c864b3a53
phuc <nguyentienphuc1991@gmail.com>
2021-12-11 22:19:23 +0700

    update code

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@ mkdir -p public
 mv besties/public/* public
 sudo rm -rf besties
 git clone -b master https://github.com/nguyentienphuc/strapi-besties.git besties
-sudo rm -rf besties/public 
 mv public/* besties/public
 sudo rm -rf public 
 cd besties
